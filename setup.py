7623071c19eea5e0e8f15460652751ac0295231e
vamonte <valentin.monte@yahoo.fr>
2015-01-19 19:54:19 +0100

    Bump to v0.1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def README():
 
 setup(
     name='django-modalview',
-    version='0.1.5-dev',
+    version='0.1.5',
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
