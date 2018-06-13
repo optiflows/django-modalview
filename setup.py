85157778b07e934c4c5715fbcf371ad97137b645
Guillaume Desvé <guillaume.desve@surycat.com>
2018-06-13 10:29:33 +0200

    Rework setup.py, use Pipfile

--- setup.py
+++ setup.py
@@ -1,35 +1,37 @@
-import os
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
-def README():
-    try:
-        import pypandoc
-        return pypandoc.convert('README.md', 'rst')
-    except (IOError, ImportError, OSError):
-        return open('README.md').read()
+
+install_reqs = parse_requirements('requirements.txt', session='dummy')
+reqs = [str(ir.req) for ir in install_reqs]
+
+try:
+    with open('VERSION.txt', 'r') as v:
+        version = v.read().strip()
+except IOError:
+    version = '0.0.0.dev0'
+
 
 setup(
     name='django-modalview',
-    version='0.1.6-dev',
+    version=version,
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
     description='Django app to add generic views.',
-    long_description=README(),
     url='https://github.com/optiflows/django-modalview',
-    author='Valentin Monte',
-    author_email='valentin.monte@optiflows.com',
-    install_requires=[
-        'django>=1.4',
-    ],
-    setup_requires=[
-        'setuptools_git>=1.0',
-        'pypandoc==0.8.3',
-    ],
+    author='Enovacom Surycat',
+    author_email='rand@surycat.com',
+    install_requires=reqs,
+    setup_requires=['setuptools_git>=1.0'],
     classifiers=[
         'Environment :: Web Environment',
-        'Framework :: Django',
+        'Framework :: Django :: 1.5',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
